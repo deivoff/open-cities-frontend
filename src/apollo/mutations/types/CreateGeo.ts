53f002a61dd46d05a62db829f1d9b9dd87425ad2
emazurov <emazurov@ozon.ru>
2020-03-21 20:26:03 +0300

    : add : types

---
+++ src/apollo/mutations/types/CreateGeo.ts
@@ -0,0 +1,32 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+import { GeometryInput, GeometryType } from "./../../../../types/globalTypes";
+
+// ====================================================
+// GraphQL mutation operation: CreateGeo
+// ====================================================
+
+export interface CreateGeo_createGeo_geometry {
+  __typename: "Geometry";
+  type: GeometryType;
+  coords: any;
+}
+
+export interface CreateGeo_createGeo {
+  __typename: "Geo";
+  _id: string;
+  geometry: CreateGeo_createGeo_geometry;
+  properties: any;
+}
+
+export interface CreateGeo {
+  createGeo: CreateGeo_createGeo;
+}
+
+export interface CreateGeoVariables {
+  geometry: GeometryInput;
+  properties: any;
+  layer: string;
+}
